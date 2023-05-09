2716391a7fdeeb24b9efdad7e7c212d02d3326ae
Gabriel Barbosa <gabrielbarbosaoliveira1999@gmail.com>
2023-05-09 20:04:54 -0300

    Iniciado projeto

---
+++ src/main/java/dev/gabrielbarbosa/glist/GListApplication.java
@@ -0,0 +1,13 @@
+package dev.gabrielbarbosa.glist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gabrielbarbosa/glist/GListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gabrielbarbosa.glist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
